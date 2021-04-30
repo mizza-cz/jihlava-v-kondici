b178761fac512de82351be93b726d0ced6104034
mizza-cz <mizza.cz666@gmail.com>
2021-04-30 19:27:37 +0200

    add first commit

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+  $('.multimedia__gallery-box').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true,
+    },
+  });
+  $('select, .checkbox').styler();
+  //presmerovani pomoci selectu
+  $('.select__redirect').change(function () {
+    location.href = $(this).val();
+  });
+});
